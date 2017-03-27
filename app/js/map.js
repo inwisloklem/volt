698a33af4e5197251a75914ba97b40c9dd4a5c9c
inwisloklem <89099109104@mail.ru>
2017-03-27 14:35:06 +0300

    Init commit

---
+++ app/js/map.js
@@ -0,0 +1,30 @@
+contactsMap = document.querySelector(".contacts__map");
+
+function initMap() {
+  var location = {lat: 56.015677, lng: 37.855522};
+  var mapOptions = {
+    center: new google.maps.LatLng(location),
+    zoom: 16,
+    disableDefaultUI: true
+  };
+
+  var map = new google.maps.Map(contactsMap, mapOptions);
+
+  var markerImage = new google.maps.MarkerImage(
+    "../img/icon-map-marker.svg",
+    new google.maps.Size(36, 36)
+  );
+
+  new google.maps.Marker({
+    position: location,
+    map: map,
+    icon: markerImage
+  });
+
+  window.onresize = function() {
+    map.setCenter(location);
+    map.setZoom(16);
+  }
+}
+
+google.maps.event.addDomListener(window, "load", initMap);
